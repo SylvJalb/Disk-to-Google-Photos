c96b98443309c0318be49ca7eafc3310429fade5
Sylvain JALBERT <sylv.jalbert@gmail.com>
2023-03-28 18:45:16 +0200

    main function for preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -3,17 +3,19 @@ import sys
 import time
 from PIL import Image
 import piexif
-import shutil
+
+#--------------------------------------------------------------#
+#---------------------------- INIT ----------------------------#
+#--------------------------------------------------------------#
 
 # Get the files directory, given in parameter
-INPUT_DIR = "./input/" + sys.argv[1]
+INPUT_DIR = "./inputs/" + sys.argv[1]
 OUTPUT_DIR = "./outputs/"
-OUTPUT_FILES_DIR = os.path.join(OUTPUT_DIR, "preprocessed_files")
-OUTPUT_BAD_DATE_LOG = os.path.join(OUTPUT_DIR, "output_bad_date.csv")
-OUTPUT_BAD_FORMAT_LOG = os.path.join(OUTPUT_DIR, "output_bad_format.csv")
+OUTPUT_BAD_DATE_LOG = os.path.join(OUTPUT_DIR, "output_bad_date.log")
+OUTPUT_BAD_FORMAT_LOG = os.path.join(OUTPUT_DIR, "output_bad_format.log")
 OUTPUT_DATAS = os.path.join(OUTPUT_DIR, "output.csv")
-OUTPUT_VIDEO_LOG = os.path.join(OUTPUT_DIR, "output_video.csv")
-OUTPUT_ERROR_LOG = os.path.join(OUTPUT_DIR, "output_error.csv")
+OUTPUT_VIDEO_LOG = os.path.join(OUTPUT_DIR, "output_video.log")
+PROCESS_LOG = os.path.join(OUTPUT_DIR, "processing.log")
 # extensions compatibles google photos
 EXTENSIONS_PHOTO = ["BMP", "GIF", "HEIC", "ICO", "JPG", "PNG", "TIFF", "WEBP", "RAW", "JPEG"]
 EXTENSIONS_VIDEO = ["3GP", "3G2", "ASF", "AVI", "DIVX", "M2T", "M2TS", "M4V", "MKV", "MMV", "MOD", "MOV", "MP4", "MPG", "MTS", "TOD", "WMV"]
@@ -21,8 +23,23 @@ EXTENSIONS_VIDEO = ["3GP", "3G2", "ASF", "AVI", "DIVX", "M2T", "M2TS", "M4V", "M
 # create output directory if not exist
 if not os.path.exists(OUTPUT_DIR):
     os.makedirs(OUTPUT_DIR)
-if not os.path.exists(OUTPUT_FILES_DIR):
-    os.makedirs(OUTPUT_FILES_DIR)
+
+# clear output files if exist
+if os.path.exists(OUTPUT_BAD_DATE_LOG):
+    os.remove(OUTPUT_BAD_DATE_LOG)
+if os.path.exists(OUTPUT_BAD_FORMAT_LOG):
+    os.remove(OUTPUT_BAD_FORMAT_LOG)
+if os.path.exists(OUTPUT_DATAS):
+    os.remove(OUTPUT_DATAS)
+if os.path.exists(OUTPUT_VIDEO_LOG):
+    os.remove(OUTPUT_VIDEO_LOG)
+if os.path.exists(PROCESS_LOG):
+    os.remove(PROCESS_LOG)
+
+
+#--------------------------------------------------------------#
+#---------------------------- FUNC ----------------------------#
+#--------------------------------------------------------------#
 
 def check_format(path):
     extension = path.split(".")[-1].upper()
@@ -33,21 +50,23 @@ def check_format(path):
     else:
         return "other"
 
-def set_date_photo(path, new_path, last_date):
-    annee = path.split(" - ")[0][-4:]
+def set_date(path, last_date, album, isPhoto):
+    annee = album[:4]
     date_creation = None
 
-    # open file
-    img = Image.open(path)
-    
-    # get date creation
-    date_creation = img.info.get("date_original")
-    if date_creation is None or date_creation == "0000:00:00 00:00:00":
-        date_creation = img.info.get("date_digitized")
+    if isPhoto:
+        # open file
+        img = Image.open(path)
+        
+        # get date creation
+        date_creation = img.info.get("date_original")
         if date_creation is None or date_creation == "0000:00:00 00:00:00":
-            date_creation = img.info.get("date_created")
-            if date_creation == "0000:00:00 00:00:00":
-                date_creation = None
+            date_creation = img.info.get("date_digitized")
+            if date_creation is None or date_creation == "0000:00:00 00:00:00":
+                date_creation = img.info.get("date_created")
+                if date_creation == "0000:00:00 00:00:00":
+                    date_creation = None
+    
     if date_creation is None and os.path.getmtime(path) is not None:
         date_creation = os.path.getmtime(path)
         if date_creation is None and os.path.getctime(path) is not None:
@@ -62,7 +81,7 @@ def set_date_photo(path, new_path, last_date):
     if year != annee:
         log_msg = ""
         # set date at the last date if it is set
-        if last_date.split(":")[0] == annee:
+        if last_date and last_date.split(":")[0] == annee:
             date_creation = last_date
             log_msg = year + " != " + annee + "\t|\tSET TO LAST DATE"
         else:
@@ -70,26 +89,27 @@ def set_date_photo(path, new_path, last_date):
             log_msg = year + " != " + annee + "\t|\tSET TO " + date_creation
         # write log
         with open(OUTPUT_BAD_DATE_LOG, 'a') as f:
-            f.write(path + "\n" + new_path + "\n\t" + log_msg + "\n\n")
+            f.write(path + "\n\t" + log_msg + "\n\n")
 
-    exif_dict = piexif.load(path)
-    # set date_creation
-    exif_dict["0th"][piexif.ImageIFD.DateTime] = date_creation
-    exif_dict["Exif"][piexif.ExifIFD.DateTimeOriginal] = date_creation
-    exif_dict["Exif"][piexif.ExifIFD.DateTimeDigitized] = date_creation
+    if isPhoto:
+        exif_dict = piexif.load(path)
+        # set date_creation
+        exif_dict["0th"][piexif.ImageIFD.DateTime] = date_creation
+        exif_dict["Exif"][piexif.ExifIFD.DateTimeOriginal] = date_creation
+        exif_dict["Exif"][piexif.ExifIFD.DateTimeDigitized] = date_creation
 
     return date_creation
 
-def save_file(path, album, id, last_date, description = ""):
+def save_file(path, album, last_date, description = ""):
     date = None
-    new_file_name = str(id) + "." + path.split(".")[-1]
-    new_path = OUTPUT_FILES_DIR + "/" + new_file_name
     # check format
     format = check_format(path)
     if format == "photo":
         # check date
-        date = set_date_photo(path, new_path, last_date)
+        date = set_date(path, last_date, album, True)
     elif format == "video":
+        # check date
+        date = set_date(path, last_date, album, False)
         # write log
         with open(OUTPUT_VIDEO_LOG, 'a') as f:
             f.write(path + "\n\t" + album + "\n\t" + description + "\n\n")
@@ -98,37 +118,48 @@ def save_file(path, album, id, last_date, description = ""):
         with open(OUTPUT_BAD_FORMAT_LOG, 'a') as f:
             f.write(path + "\n\t" + album + "\n\t" + description + "\n\n")
     else :
-        # copy file
-        try:
-            shutil.copy2(path, new_path)
-        except:
-            # write log
-            with open(OUTPUT_ERROR_LOG, 'a') as f:
-                f.write(path + "\n\t" + album + "\n\t" + description + "\n\n")
-        # write in OUTPUT_DATAS csv file
+        # write infos in the data file
         with open(OUTPUT_DATAS, 'a') as f:
-            f.write(str(id) + ";" + album + ";" + new_file_name + ";" + path + ";" + new_path + ";" + description + ";" + date)
+            f.write(path + ";" + album + ";" + description + ";" + date + "\n")
     return date
 
+
+#--------------------------------------------------------------#
+#---------------------------- MAIN ----------------------------#
+#--------------------------------------------------------------#
+
+# get albums in the input directory
 albums = []
-# parcour du dossier Photos
 for elt in os.listdir(INPUT_DIR):
     albums.append(elt)
+
 # order folder by number asc
 albums.sort(key=lambda x: int(x[:4]))
 
-for album in albums:
-    print("Album : " + album)
-    # parcour du dossier album
-    for elt in os.listdir(INPUT_DIR + "/" + album):
-        # si c'est un fichier
-        if os.path.isfile(INPUT_DIR + "/" + album + "/" + elt):
-            # save file
-            save_file(INPUT_DIR + "/" + album + "/" + elt, album)
-        # si c'est un dossier
-        else:
-            for elt_elt in os.listdir(INPUT_DIR + "/" + album + "/" + elt):
-                # si c'est un fichier
-                if os.path.isfile(INPUT_DIR + "/" + album + "/" + elt + "/" + elt_elt):
-                    # save file
-                    save_file(INPUT_DIR + "/" + album + "/" + elt + "/" + elt_elt, album, elt)
+last_date = "0000:00:00 00:00:00"
+
+for index, album in enumerate(albums):
+    with open(PROCESS_LOG, 'a') as log:
+        log.write("Processing \"" + album + "\" -> " + str(index + 1) + "/" + str(len(albums)) + " ...\n")
+        album_path = os.path.join(INPUT_DIR, album)
+        number_of_files = sum([len(files) for r, d, files in os.walk(album_path)])
+        counter = 1
+        # parcour du dossier album
+        for elt in os.listdir(album_path):
+            elt_path = os.path.join(album_path, elt)
+            # si c'est un fichier
+            if os.path.isfile(elt_path):
+                log.write("\tProcessing \"" + album + "\" -> " + str(index + 1) + "/" + str(len(albums)) + " ... " + str(counter) + "/" + str(number_of_files) + "\n")
+                # save file
+                last_date = save_file(elt_path, album, last_date)
+                counter += 1
+            # si c'est un dossier
+            else:
+                for elt_elt in os.listdir(elt_path):
+                    elt_elt_path = os.path.join(elt_path, elt_elt)
+                    # si c'est un fichier
+                    if os.path.isfile(elt_elt_path):
+                        log.write("\tProcessing \"" + album + "\" -> " + str(index + 1) + "/" + str(len(albums)) + " ... " + str(counter) + "/" + str(number_of_files) + "\n")
+                        # save file
+                        last_date = save_file(elt_elt_path, album, last_date, elt)
+                        counter += 1
